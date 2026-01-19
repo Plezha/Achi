84711d3a2c921db170b87c65296331255c9dde5a
Artyom Plekhanov <plehanovtemka@gmail.com>
2026-01-19 18:58:46 +0300

    Sign release build

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -167,16 +167,20 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
